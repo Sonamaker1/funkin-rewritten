ad911f86fbf0da34d3bc467a321a45ab95fb1a60
Sonamaker1 <87080176+Sonamaker1@users.noreply.github.com>
2021-07-18 16:38:35 -0400

    Minor input system change to submenus
    Up and down have been added in addition to left and right on all submenus besides the first menu

--- src/love/menu.lua
+++ src/love/menu.lua
@@ -89,7 +89,7 @@ local music = love.audio.newSource("music/menu/menu.ogg", "stream")
 logo.x, logo.y = -350, -125
 logo.sizeX, logo.sizeY = 1.25, 1.25
 
-girlfriendTitle.x, girlfriendTitle.y = 300, -75
+girlfriendTitle.x, girlfriendTitle.y = 300*3/2, -205*3/2
 
 music:setLooping(true)
 
@@ -110,8 +110,22 @@ menu = {
 	update = function(dt)
 		girlfriendTitle:update(dt)
 		
+		local quickInputs = {
+			left = input:pressed("left"),
+			right = input:pressed("right"),
+			up = input:pressed("up"),
+			down = input:pressed("down"),
+			confirm = input:pressed("confirm"),
+			back = input:pressed("back")
+		}
+
+		if menuState > 0 then
+			quickInputs.left  = quickInputs.left or quickInputs.up
+			quickInputs.right = quickInputs.right or quickInputs.down 
+		end
+
 		if not graphics.isFading then
-			if input:pressed("left") then
+			if quickInputs.left then
 				audio.playSound(selectSound)
 				
 				if menuState == 2 then
@@ -133,7 +147,7 @@ menu = {
 						weekNum = maxNumberOfWeeks
 					end
 				end
-			elseif input:pressed("right") then
+			elseif quickInputs.right then
 				audio.playSound(selectSound)
 				
 				if menuState == 2 then
@@ -155,7 +169,7 @@ menu = {
 						weekNum = 1
 					end
 				end
-			elseif input:pressed("confirm") then
+			elseif quickInputs.confirm then
 				audio.playSound(confirmSound)
 				
 				menuState = menuState + 1
@@ -182,7 +196,7 @@ menu = {
 						end
 					)
 				end
-			elseif input:pressed("back") then
+			elseif quickInputs.back then
 				if menuState > 0 then -- Don't play sound if exiting the game
 					audio.playSound(selectSound)
 				end
@@ -208,9 +222,13 @@ menu = {
 			
 			logo:draw()
 			
+			love.graphics.push()
+			love.graphics.scale(cam.sizeX*2/3, cam.sizeY*2/3)
 			girlfriendTitle:draw()
+			love.graphics.pop()
 			
-			love.graphics.printf("By HTV04\nv1.0.0 beta 3\n\nOriginal game by ninjamuffin99, PhantomArcade, kawaisprite, and evilsk8er, in association with Newgrounds", -525, 90, 450, "right", nil, 1, 1)
+			love.graphics.printf("\t\t\tBy HTV04\t\tv1.0.0 beta 3", -625, 90, 900, "left", nil, 1, 1)
+			love.graphics.printf("Original game by ninjamuffin99, PhantomArcade, kawaisprite, and evilsk8er, \nin association with Newgrounds", -625, 350, 1200, "left", nil, 1, 1)
 			
 			graphics.setColor(1, 1, 0)
 			if menuState == 2 then
@@ -222,28 +240,47 @@ menu = {
 					love.graphics.printf("Choose a difficulty: < Hard >", -640, 285, 853, "center", nil, 1.5, 1.5)
 				end
 			elseif menuState == 1 then
+				graphics.setColor(0, 1, 1)
+				love.graphics.push()
+				love.graphics.translate(0,-140)
+				for i = 1, #weekSongs[weekNum] do 
+					local selected = i == songNum
+					local sign = (function() if selected then return ' >' else return'  ' end end)()
+					love.graphics.printf("" .. sign .. weekSongs[weekNum][i] , -640, 305+(i)*40, 853, "center", nil, 1.5, 1.5)
+				end 
+				graphics.setColor(1, 1, 0)
+				local sign = (function() if songNum == 0 then return ' >' else return'  ' end end)()
+				love.graphics.printf("" .. sign .. "Play Story Mode" , -640*4/3, 300, 853, "center", nil, 2, 2)
+
+				love.graphics.pop()
 				if songNum == 0 then
-					love.graphics.printf("Choose a song: < (Story Mode) >", -640, 285, 853, "center", nil, 1.5, 1.5)
+					love.graphics.printf("Choose a song: \n< (Story Mode) >", -640-100, 285-80, 453, "center", nil, 1.5, 1.5)
 				else
-					love.graphics.printf("Choose a song: < " .. weekSongs[weekNum][songNum] .. " >", -640, 285, 853, "center", nil, 1.5, 1.5)
+					love.graphics.printf("Choose a song: \n< Freeplay: " .. weekSongs[weekNum][songNum] .. " >", -640-100, 285-80, 453, "center", nil, 1.5, 1.5)
 				end
+				
+				
 			elseif menuState == 0 then
+				love.graphics.push()
+				love.graphics.translate(200,-50)
+				graphics.setColor(1, 1, 0)
 				love.graphics.printf("Choose a week: \n < " .. weekIDs[weekNum] .. " >", -540, 265, 853, "left", nil, 1.5, 1.5)
 				storyMenuImages[weekNum]:draw()
+				love.graphics.pop()
 			end
 			graphics.setColor(1, 1, 1)
 			
 			if menuState <= 0 then
 				if input:getActiveDevice() == "joy" then
-					love.graphics.printf("Left Stick/D-Pad: Select | A: Confirm | B: Exit", -640, 350, 1280, "center", nil, 1, 1)
+					love.graphics.printf("Left Stick/D-Pad: Select | A: Confirm | B: Exit", -640, 120, 1280, "center", nil, 1, 1)
 				else
-					love.graphics.printf("Arrow Keys: Select | Enter: Confirm | Escape: Exit", -640, 350, 1280, "center", nil, 1, 1)
+					love.graphics.printf("Arrow Keys: Select | Enter: Confirm | Escape: Exit", -640, 120, 1280, "center", nil, 1, 1)
 				end
 			else
 				if input:getActiveDevice() == "joy" then
-					love.graphics.printf("Left Stick/D-Pad: Select | A: Confirm | B: Back", -640, 350, 1280, "center", nil, 1, 1)
+					love.graphics.printf("Left Stick/D-Pad: Select | A: Confirm | B: Back", -640, 120, 1280, "center", nil, 1, 1)
 				else
-					love.graphics.printf("Arrow Keys: Select | Enter: Confirm | Escape: Back", -640, 350, 1280, "center", nil, 1, 1)
+					love.graphics.printf("Arrow Keys: Select | Enter: Confirm | Escape: Back", -640, 120, 1280, "center", nil, 1, 1)
 				end
 			end
 		love.graphics.pop()
